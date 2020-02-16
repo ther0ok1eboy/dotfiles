59547c7959ed8d5c42a07fe356aea7ab54237c3d
coolshark404 <jason10032992@gmail.com>
2020-02-16 19:23:52 +0800

    version 2.0...

--- i3_config/init/startup.sh
+++ i3_config/init/startup.sh
@@ -11,10 +11,14 @@ cat << eof
 *   1:) initialize system             *
 *   2:) connect network               *
 *   3:) recycle rubbish               *
-*   4:) go to github                  *
+*   4:) auto-update system            *
 *   5:) optmize deepin-tim            *
-*   6:) show menu                     *
-*   7:) quit                          *
+*   6:) network speedtest             *
+*   7:) startup VNC service           *
+*   8:) load ssh agent                *
+*   9:) power off                     *
+*  10:) show menu                     *
+*  11:) quit                          *
 ***************************************
 **${writer}          ${date}**
 ***************************************
@@ -40,7 +44,7 @@ while true; do
         clear
         cowsay -f dragon welcome to manjaro world
         sudo mount /dev/sda4 ~/new-world
-        echo "disk have been mounted...."
+        echo "disk have been mounted...." | lolcat
         sleep 3
         sudo modprobe -a vmw_vmci vmmon
         echo "vmware workstation initialize completely..." | lolcat
@@ -59,25 +63,23 @@ while true; do
         if [ $isConnect == 'y' ]; then
             echo "yes"
             sudo ip link set enp2s0 down
-            echo "it is maybe spends some times..."
+            echo "it maybe spends some times..." | lolcat
+            sudo netctl start pppoe
             #test whether or not there is network
             while true; do
             ping -c 3 -n -w5 www.baidu.com >> tempt.txt
-            for i in `cat b.txt | sed -n '2'p`; do
+            for i in `cat tempt.txt | sed -n '2'p`; do
                 sleep 1
                 if [[ $i != "" ]]; then
-                    if [ -e b.txt ]; then
-                        mv temp.txt /rubbish
+                    if [ -e tempt.txt ]; then
+                        mv tempt.txt /rubbish
                     fi    
-                    echo "success to connect network..."
+                    echo "success to connect network..." | lolcat
                     exit
                 fi
                 done
             sleep 2
             done
-
-            sudo netctl start pppoe
-            echo "connect network successfully by pppoe protocol..."
         else
             echo "nothing to do..."
         fi
@@ -94,9 +96,9 @@ while true; do
                 echo "recycle rubbish bin successfully..."
             fi
         else
-            read -p "you have not rubbish bin, do you want to build a rubbish bin?(y/n) isBuild"
+            read -p "you have not rubbish bin, do you want to build a rubbish bin?(y/n)" isBuild
             if [ $isBuild == "y" ]; then
-                sudo mkdir /rubbish
+                sudo mkdir -m 777 /rubbish
                 echo "build successfully..."
             else
                 echo "nothing to do"
@@ -104,34 +106,55 @@ while true; do
         fi
         ;;
     
-    #*   4:) go to github
+    #*   4:) update
         4)
-        if [ -e ~/documents/github-local/manjaro_config ]; then
-            cd ~/documents/github-local/manjaro_config
-            echo "maybe this function have some bugs to execute..."
-        else
-            echo "this directory not exist..."
-        fi
+        sudo pacman -Syu -y
+        echo "system update successfully..."
         ;;
     
     #*   5:) optimize deepin-tim  
         5)
         read -p "if execute this function, this menu will hang out there to become daemon for deepin-TIM, whether or not to continue execute it?(y/s) " isExec
     
-        if [ $isExec="y" ]; then
+        if [ $isExec == "y" ]; then
             echo "TIM environments are built completely..."
             /usr/lib/gsd-xsettings
         else
             echo "nothing to do..."
         fi
         ;;
-    
-        6)
+
+        6) 
+        speedtest-cli --bytes 
+        ;;  
+
+        7) 
+        x11vnc -nap -wait 50 -noxdamage -rfbauth /home/sorria/.vnc/passwd -display :0 -nocursor -forever -o /home/sorria/.vnc/x11vnc.log -bg
+        echo "x11vnc.service startup successfully..."
+        read -p "Do you want to view the x11vnc log?(y/s)" tag
+        if [ $tag == "y" ]; then 
+            cat ~/.vnc/x11vnc.log | less
+        else 
+            echo "have nothing to do..."
+        fi
+        ;;  
+
+        8)
+        read -p "please input the port you want to connect(recommend 10222 or 7233)" port
+        ssh -CqTfnN -R :$port:localhost:22 root@49.235.215.98 -p 10022       
+        echo "ssh agent load successfully..."
+        ;;  
+
+        9) 
+        bash /usr/local/poweroff.sh
+        ;;  
+
+        10)
         clear
         menu
         ;;
     
-        7)
+        11)
         echo "quit..."
         break
         ;;
@@ -141,5 +164,7 @@ while true; do
         ;;  
     esac
 done
+
+
 
 
